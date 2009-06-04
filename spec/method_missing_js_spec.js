1859f079ac2b0b4d10bc3e0cddcafefa4afb214a
Pat Nakajima <patnakajima@gmail.com>
2009-06-03 20:16:24 -0400

    grr, this can never work

--- spec/method_missing_js_spec.js
+++ spec/method_missing_js_spec.js
@@ -25,13 +25,35 @@ Screw.Unit(function() {
     });
 
     it("delegates missing method calls to methodMissing", function() {
-      MM(receiver, function() { this.bar(); });
+      MM(receiver, function() {
+        this.bar();
+        this.fizz();
+      });
       expect(receiver.calls[0]).to(equal, 'bar');
+      expect(receiver.calls[1]).to(equal, 'fizz');
     });
 
     it("removes methodMissing from receiver after", function() {
       MM(receiver, function() { this.bar(); });
       expect(receiver.bar).to(be_undefined);
+    });
+
+    it("allows bracket calls", function() {
+      MM(receiver, function() {
+        this['fizz']();
+        this['buzz']();
+      });
+      expect(receiver.calls[0]).to(equal, 'fizz');
+      expect(receiver.calls[1]).to(equal, 'buzz');
+    });
+
+    it("mixes and matches", function() {
+      MM(receiver, function() {
+        this['buzz']();
+        this.fizz();
+      });
+      expect(receiver.calls[1]).to(equal, 'buzz');
+      expect(receiver.calls[0]).to(equal, 'fizz');
     });
 
     it("only delegates when methodMissing exists", function() {
--- src/method_missing_js.js
+++ src/method_missing_js.js
@@ -1,26 +1,74 @@
 // method-missing-js
 (function($) {
-  function MM(receiver, fn, replaced) {
+  // Find this.method() calls
+  function parseDot(string) {
+    var match = string.match(/this\.(\w+)/g) || [];
+    var i = match.length;
+    var results = [];
+    while(i-- > 0) { results.push(match[i].split('.')[1]); }
+    console.info('dot: ' + results.join(', '));
+    return results.reverse();
+  }
+
+  // Find this['method']() calls
+  function parseBracket(string) {
+    var clean = string.split("['").join('["').split("']").join('"]');
+    var match = clean.match(/this\[\"(\w+)\"\]/g) || [];
+    var i = match.length;
+    var results = [];
+    while(i-- > 0) { results.push(match[i].match(/this\[\"(\w+)\"\]/)[1]); }
+    console.info('bracket: ' + results.join(', '));
+    return results.reverse();
+  }
+
+  function MM(receiver, fn, replaced, recalled) {
     replaced = replaced || [];
+    
     try { fn.call(receiver); }
     catch(e) {
 
       // If methodMissing not defined, just re-throw error.
       if (!receiver.methodMissing) { throw(e); }
 
-      // Find the missing key...
-      var missed = fn.toString().match(/this\.(\w+)/)[1];
+      var missed;
+      var cleaned = fn.toString();
 
-      // Add it to list of keys replaced so far...
-      replaced.push(missed);
+      function replaceKey(key) {
+        // Find the missing key...
+        if (replaced.indexOf(key) > -1) {
+          return;
+        }
 
-      // Define key to use method missing.
-      receiver[missed] = function() {
-        var a = [missed];
-        var i = arguments.length;
-        while(i--) { a.push(arguments[i]); }
-        receiver.methodMissing.apply(receiver, a);
+        missed = key;
+
+        // Add it to list of keys replaced so far...
+        replaced.unshift(missed);
+        
+        console.info(missed);
+        
+        // Define key to use method missing.
+        receiver[missed] = function() {
+          if (replaced[0] == missed) {
+            var a = [missed];
+            if (i = arguments.length) { while(i--) { a.push(arguments[i]); } }
+            receiver.methodMissing.apply(receiver, a);
+          }
+        }
       }
+
+      var keys = parseDot(cleaned);
+      var i = keys.length;
+      while(i--) { replaceKey(keys[i]) }
+
+      console.info(replaced);
+
+      var keys = parseBracket(cleaned);
+      var i = keys.length;
+      while(i--) { replaceKey(keys[i]) }
+      
+      console.info(replaced);
+
+      if (!missed) { throw(e); }
 
       // Try again.
       MM(receiver, fn, replaced)
